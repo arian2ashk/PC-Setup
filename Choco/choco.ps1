63dad2ede75250369729a2969cc3f222eac03394
Arian Kadkhoda <arian.2ashk@gmail.com>
2021-04-30 12:32:38 +0200

    Update choco.ps1

--- Choco/choco.ps1
+++ Choco/choco.ps1
@@ -6,34 +6,35 @@ choco feature enable -n=allowGlobalConfirmation
 
 choco install googlechrome
 choco install vscode
-choco install visualstudio2019enterprise
+#choco install visualstudio2019enterprise
+#choco install visualstudio2019professional
 choco install conemu
 choco install git
 choco install notepadplusplus
-choco install spotify
+#choco install spotify
 choco install adobereader
 choco install steam
 #choco install virtualbox
-#choco install firefox
+choco install firefox
 #choco install linqpad
-#choco install postman
+choco install postman
 #choco install screentogif
 choco install telegram
 choco install resharper
 choco install 7zip
-choco install ravendb #--params="'/setup-as-service /start-service'"
+#choco install ravendb #--params="'/setup-as-service /start-service'"
+#choco install sql-server-2019
 #choco install sql-server-management-studio
 choco install kdiff3
 choco install nodejs-lts
-#choco install sql-server-2019
 choco install nordvpn
 choco install logitechgaming
 choco install geforce-experience
-choco install ngrok
+#choco install ngrok
 choco install hackfont
-choco install microsoft-teams
+#choco install microsoft-teams
 #choco install powertoys
-choco install zoom
+#choco install zoom
 choco install docker-desktop
 choco install discord
 
@@ -42,10 +43,11 @@ choco pin add -n=googlechrome
 choco pin add -n=vscode
 choco pin add -n="vscode.install"
 choco pin add -n=visualstudio2019enterprise
+choco pin add -n=visualstudio2019professional
 choco pin add -n=conemu
 choco pin add -n=spotify
 choco pin add -n=steam
-#choco pin add -n=firefox
+choco pin add -n=firefox
 choco pin add -n=telegram
 choco pin add -n="telegram.install"
 choco pin add -n=resharper
