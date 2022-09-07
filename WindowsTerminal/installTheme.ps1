186e01cfe311834909db1ed7c147ae8656618ac7
Arian <arian.2ashk@gmail.com>
2022-09-07 14:38:12 +0200

    updated script

--- WindowsTerminal/installTheme.ps1
+++ WindowsTerminal/installTheme.ps1
@@ -1,7 +1,8 @@
 Set-ExecutionPolicy Bypass;
 
 # the file path and name is used in pwsh profile, be careful if you change it
-(New-Object System.Net.WebClient).DownloadFile('https://gist.githubusercontent.com/shanselman/1f69b28bfcc4f7716e49eb5bb34d7b2c/raw/8e9c9a8736ff4e9e5a863c20833d614549ccbc32/ohmyposhv3-v2.json', "$env:POSH_THEMES_PATH\ohmyposhv3-v2.json")
+# (New-Object System.Net.WebClient).DownloadFile('https://gist.githubusercontent.com/shanselman/1f69b28bfcc4f7716e49eb5bb34d7b2c/raw/8e9c9a8736ff4e9e5a863c20833d614549ccbc32/ohmyposhv3-v2.json', "$env:POSH_THEMES_PATH\ohmyposhv3-v2.json")
+pwsh -command {Copy-Item -Path ".\ohmyposhv3-v2.json" -Destination "$env:POSH_THEMES_PATH\" -force}
 
 # create a pwsh profile that is digitally signed
 pwsh -command {New-Item -Path $PROFILE -Type File -Force}
